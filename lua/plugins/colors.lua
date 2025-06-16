7318c47002091649d76c22187f8ef3ba91056a37
stjohnvincent <jvbadcaps@gmail.com>
2025-06-17 00:46:20 +0800

    initial nvim config files

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,33 @@
+local function enable_transparency()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+end
+return {
+    --{
+    --    "folke/tokyonight.nvim",
+    --    config = function()
+    --        vim.cmd.colorscheme "tokyonight"
+    --        enable_transparency()
+    --    end
+    --},
+    {
+        "Mofiqul/vscode.nvim",
+        name = 'vscode',
+        config = function()
+            vim.cmd.colorscheme "vscode"
+            vim.cmd('hi Directory guibg=NONE')
+            vim.cmd('hi SignColumn guibg=NONE')
+            enable_transparency()
+        end
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = 'tokyonight',
+        }
+    }
+}
